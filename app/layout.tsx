21cfe48ab6e51a0084aa8835750f355a5b808614
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-15 18:53:29 -0300

    style: load content completion stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./architecture.css";
 import "./home-extra.css";
 import "./refinements.css";
+import "./content-completion.css";
 import { LazyReveal } from "./components/LazyReveal";
 
 export const metadata: Metadata = {
