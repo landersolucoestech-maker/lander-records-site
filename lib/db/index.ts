138ada498a654a20b460a334c2462b95724968bb
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-20 03:37:49 -0300

    refactor: register artist management schema

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
+import * as coreSchema from "./schema";
+import * as artistManagementSchema from "./artist-management-schema";
+
+const schema = { ...coreSchema, ...artistManagementSchema };
 
 let client: ReturnType<typeof postgres> | null = null;
 type Database = ReturnType<typeof drizzle<typeof schema>>;
