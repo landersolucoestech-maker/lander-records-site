87355a00ff3aca09b14716c14208ebc97f059511
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-16 01:05:13 -0300

    refactor(artists): use reference shell for new artist flow

--- app/admin/(protected)/artists/new/page.tsx
+++ app/admin/(protected)/artists/new/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import ArtistForm from "../ArtistForm";
 import { loadArtistOptions } from "../editor-data";
 
@@ -6,13 +5,5 @@ export const dynamic = "force-dynamic";
 
 export default async function NewArtistPage() {
   const options = await loadArtistOptions();
-  return (
-    <div className="adminPage">
-      <header className="adminPageHeader">
-        <div><p className="adminEyebrow">ARTISTAS</p><h1>Novo artista</h1><p>Cadastre identidade, funções, gênero, métricas, plataformas, mídia e destinos de publicação em um único fluxo.</p></div>
-        <Link className="adminButton" href="/admin/artists">Voltar</Link>
-      </header>
-      <ArtistForm {...options} />
-    </div>
-  );
+  return <div className="adminPage"><ArtistForm {...options} /></div>;
 }
