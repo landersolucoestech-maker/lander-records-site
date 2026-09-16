45834792dc6e6657c1ab3e781ebd8115c77d4373
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-16 17:00:58 -0300

    fix(admin): remove standalone new content route

--- app/admin/(protected)/posts/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostForm from "../PostForm";
-import { loadPostOptions } from "../editor-data";
-
-export const dynamic = "force-dynamic";
-
-export default async function NewPostPage() {
-  const options = await loadPostOptions();
-  return <div className="adminPage"><PostForm {...options} /></div>;
-}
