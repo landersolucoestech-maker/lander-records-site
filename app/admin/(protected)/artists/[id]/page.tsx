2b0415c95d28a36a4df4f1695b789f9cd53bef72
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-09-16 01:05:05 -0300

    refactor(artists): remove legacy duplicate editor heading

--- app/admin/(protected)/artists/[id]/page.tsx
+++ app/admin/(protected)/artists/[id]/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import ArtistForm from "../ArtistForm";
 import { loadArtistEditor, loadArtistOptions } from "../editor-data";
@@ -10,14 +9,8 @@ export default async function EditArtistPage({ params, searchParams }: { params:
   const [{ saved }, initial, options] = await Promise.all([searchParams, loadArtistEditor(id), loadArtistOptions()]);
   if (!initial) notFound();
 
-  return (
-    <div className="adminPage">
-      <header className="adminPageHeader">
-        <div><p className="adminEyebrow">ARTISTA</p><h1>{initial.name}</h1><p>Edite identidade, publicação, métricas, plataformas, mídia e conteúdo público sem misturar responsabilidades.</p></div>
-        <div className="adminActions"><Link className="adminButton" href={`/admin/artists/${id}/view`}>Visualizar</Link><Link className="adminButton" href="/admin/artists">Voltar</Link></div>
-      </header>
-      {saved ? <div className="adminAlert">Alterações salvas com sucesso.</div> : null}
-      <ArtistForm initial={initial} {...options} />
-    </div>
-  );
+  return <div className="adminPage">
+    {saved ? <div className="adminAlert">Alterações salvas com sucesso.</div> : null}
+    <ArtistForm initial={initial} {...options} />
+  </div>;
 }
